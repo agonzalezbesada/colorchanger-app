30710e3d96e146e3b06bdd5bdca507a8ef310ea6
Adrian <adriangonbes@gmail.com>
2024-03-21 12:08:53 +0100

    Color Changer without copy

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import ColorChanger from './components/ColorChanger';
+import './ColorChanger.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='content-web'>
+        <ColorChanger />
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ColorChanger.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+
+export default function ColorChanger() {
+    const[bgColor, setBgColor] = useState("#FFFFFF");
+
+
+    function hexColor() {
+        setBgColor("#" +
+        Math.floor(Math.random() * 256).toString(16) +
+        "" +
+        Math.floor(Math.random() * 256).toString(16) +
+        "" +
+        Math.floor(Math.random() * 256).toString(16));
+    }
+
+
+
+
+  // style={{backgroundColor: `${bgColor}`}}
+
+  return (
+    <div className="color-changer-container" style={{backgroundColor: `${bgColor}`}}>
+      <h1 className="color-changer-title">
+        Change The Background By Clicking The Following Button
+      </h1>
+      <div className="hex-display">
+        <input type="text" name="hex-field" id="hex-field" value={bgColor.toString()} />
+        <button className="btn-copy" style={{backgroundColor: `${bgColor}`}}>copy</button>
+      </div>
+      <button
+        className="btn-change"
+        onClick={hexColor}
+      >
+        CHANGE COLOR
+      </button>
+    </div>
+  );
+}
